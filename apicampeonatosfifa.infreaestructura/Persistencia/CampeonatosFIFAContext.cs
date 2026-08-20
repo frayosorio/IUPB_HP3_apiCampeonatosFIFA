2c74cf45ce52377b9c0b190f3fea992efd234600
unknown <frayosorio@gmail.com>
2026-08-20 14:55:15 -0500

    Se agrega capa de INFRAESTRUCTURA

---
+++ apicampeonatosfifa.infreaestructura/Persistencia/CampeonatosFIFAContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace apicampeonatosfifa.infraestructura.Persistencia
+{
+    public class CampeonatosFIFAContext : DbContext
+    {
+
+    }
+}
